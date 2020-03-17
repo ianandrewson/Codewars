95c4e3430a50d76acc825d941a8aef925f1cf701
Ian Andrewson <ianandrewson@gmail.com>
2020-03-17 14:00:05 -0700

    linted

--- 5 Kyu/findOdd/findOdd.js
+++ 5 Kyu/findOdd/findOdd.js
@@ -4,28 +4,23 @@
 
 
 function findOdd(arr) {
-    const checkedValues = [];
-    let result;
+  const checkedValues = [];
+  let result;
     
-    arr.forEach(num => {
-        if (!checkedValues.includes(num)) {
-            checkedValues.push(num);
-            const newNumberCount = arr.reduce((count, numberToCount) => {
-                if (numberToCount === num) {
-                    return count + 1;
-                } else return count;
-            }, 0);
-            if (newNumberCount % 2 === 1) {
-                result = num;
-            } 
-        }
-    });
-    return result;
+  arr.forEach(num => {
+    if(!checkedValues.includes(num)) {
+      checkedValues.push(num);
+      const newNumberCount = arr.reduce((count, numberToCount) => {
+        if(numberToCount === num) {
+          return count + 1;
+        } else return count;
+      }, 0);
+      if(newNumberCount % 2 === 1) {
+        result = num;
+      } 
+    }
+  });
+  return result;
 }
-
-const reducer = (accumulator, currentValue) => {
-
-};
-
 
 module.exports = findOdd;
