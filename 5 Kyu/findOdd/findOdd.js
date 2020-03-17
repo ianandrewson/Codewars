fd5986694bddfe06119ad79561b899f19f93bf2a
Ian Andrewson <ianandrewson@gmail.com>
2020-03-17 12:44:32 -0700

    added dependencies. Test written for findOdd, ready to TDD

---
+++ 5 Kyu/findOdd/findOdd.js
@@ -0,0 +1,11 @@
+// Given an array, find the integer that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+
+function findOdd(arr) {
+  
+    return 0;
+}
+
+export default findOdd;
--- 5 Kyu/findOddOccuringInt.js
+++
@@ -1,9 +0,0 @@
-// Given an array, find the integer that appears an odd number of times.
-
-// There will always be only one integer that appears an odd number of times.
-
-
-function findOdd(arr) {
-  
-    return 0;
-}
