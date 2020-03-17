0f673add1d53015adbd95aec2e0521f766d40526
Ian Andrewson <ianandrewson@gmail.com>
2020-03-17 13:59:06 -0700

    findOdd implemented, all tests pass

--- 5 Kyu/findOdd/findOdd.js
+++ 5 Kyu/findOdd/findOdd.js
@@ -4,8 +4,28 @@
 
 
 function findOdd(arr) {
+    const checkedValues = [];
+    let result;
     
-  return arr;
+    arr.forEach(num => {
+        if (!checkedValues.includes(num)) {
+            checkedValues.push(num);
+            const newNumberCount = arr.reduce((count, numberToCount) => {
+                if (numberToCount === num) {
+                    return count + 1;
+                } else return count;
+            }, 0);
+            if (newNumberCount % 2 === 1) {
+                result = num;
+            } 
+        }
+    });
+    return result;
 }
+
+const reducer = (accumulator, currentValue) => {
+
+};
+
 
 module.exports = findOdd;
