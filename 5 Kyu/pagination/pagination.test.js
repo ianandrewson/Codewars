40fbb323fcd47a611909667982b3ae14ffa478a2
Ian Andrewson <ianandrewson@gmail.com>
2020-03-23 11:39:45 -0700

    all tests written for PaginationHelper methods. Ready to TDD

--- 5 Kyu/pagination/pagination.test.js
+++ 5 Kyu/pagination/pagination.test.js
@@ -14,4 +14,27 @@ describe('pagination tests', () => {
     expect(paginator.pageItemCount).toBeDefined();
     expect(paginator.pageIndex).toBeDefined();
   });
+
+  it('can count the number of items in the collection', () => {
+    expect(paginator.itemCount()).toEqual(6);
+  });
+
+  it('can determine the number of pages necessary to display all it', () => {
+    expect(paginator.pageCount()).toEqual(2);
+  });
+
+  it('can determine how many pages per item there should be on any given page', () => {
+    expect(paginator.pageItemCount(0)).toEqual(4);
+    expect(paginator.pageItemCount(1)).toEqual(2);
+    expect(paginator.pageItemCount(2)).toEqual(-1);
+  });
+
+  it('can determine which page an item is on via its index', () => {
+    expect(paginator.pageIndex(5)).toEqual(1);
+    expect(paginator.pageIndex(4)).toEqual(1);
+    expect(paginator.pageIndex(0)).toEqual(0);
+    expect(paginator.pageIndex(2)).toEqual(0);
+    expect(paginator.pageIndex(20)).toEqual(-1);
+    expect(paginator.pageIndex(-10)).toEqual(-1);
+  });
 });
